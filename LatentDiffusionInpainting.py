d430f12f1772f94f45f4eb1e179fcd33674355ae
Maatra <ccharest93@hotmail.com>
2023-06-21 15:13:01 +0100

    sampling done

--- LatentDiffusionInpainting.py
+++ LatentDiffusionInpainting.py
@@ -5,6 +5,8 @@ import open_clip
 import torch
 import numpy as np
 from einops import repeat    
+from functools import partial
+from tqdm import tqdm
 def pad_image(input_image):
     pad_w, pad_h = np.max(((2, 2), np.ceil(
         np.array(input_image.size) / 64).astype(int)), axis=0) * 64 - input_image.size
@@ -39,73 +41,158 @@ def make_batch_sd(
     }
     return batch
 
+def noise_like(shape, device, repeat=False):
+    repeat_noise = lambda: torch.randn((1, *shape[1:]), device=device).repeat(shape[0], *((1,) * (len(shape) - 1)))
+    noise = lambda: torch.randn(shape, device=device)
+    return repeat_noise() if repeat else noise()
+def extract_into_tensor(a, t, x_shape):
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
 class LatentDiffusionInpainting(nn.Module):
-    def __init__(self):
+    def __init__(self, ckpt_path):
         super().__init__()
-        #self.diffusion_model = Unet()
+        self.parameterization = 'eps' 
+        self.image_size = 64
+        self.channels = 4
+        self.diffusion_model = Unet()
+        #diffusion params
+        self.dpm_num_timesteps = 1000
+        self.diffusion_param_schedule()
+        timesteps, = self.betas.shape
+        self.num_timesteps = int(timesteps)
+        self.scale_factor = 0.18215
+        #submodels init
         self.first_stage_model = AutoencoderKL()
         self.cond_stage_model, _, _ = open_clip.create_model_and_transforms("ViT-H-14", device=torch.device('cpu'), pretrained="laion2b_s32b_b79k")
         del self.cond_stage_model.visual
         self.max_length = 77
-        self.scale_factor = 0.18215
-        self.channels = 4
-
-    def forward(self, x):
-
-        return
+        #---------- init latent diffusion end
+        self.concat_keys = ('mask', 'masked_image')
+        self.masked_image_key = 'masked_image'
+    def diffusion_param_schedule(self):
+        v_posterior = 0.0
+        linear_start=1e-4
+        linear_end=2e-2
+        betas = (torch.linspace(linear_start ** 0.5, linear_end ** 0.5, 1000, dtype=torch.float64) ** 2).numpy()
+        alphas = 1. - betas
+        alphas_cumprod = np.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = np.append(1., alphas_cumprod[:-1])
+        to_torch = partial(torch.tensor, dtype=torch.float32)
+        self.betas = to_torch(betas)
+        self.alphas_cumprod = to_torch(alphas_cumprod)
+        self.alphas_cumprod_prev = to_torch(alphas_cumprod_prev)
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.sqrt_alphas_cumprod = to_torch(np.sqrt(alphas_cumprod))
+        self.sqrt_one_minus_alphas_cumprod = to_torch(np.sqrt(1. - alphas_cumprod))
+        self.log_one_minus_alphas_cumprod = to_torch(np.log(1. - alphas_cumprod))
+        self.sqrt_recip_alphas_cumprod = to_torch(np.sqrt(1. / alphas_cumprod))
+        self.sqrt_recipm1_alphas_cumprod = to_torch(np.sqrt(1. / alphas_cumprod - 1))
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        posterior_variance = (1 - v_posterior) * betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod) + v_posterior * betas
+        # above: equal to 1. / (1. / (1. - alpha_cumprod_tm1) + alpha_t / beta_t)
+        self.posterior_variance = to_torch(posterior_variance)
+        # below: log calculation clipped because the posterior variance is 0 at the beginning of the diffusion chain
+        self.posterior_log_variance_clipped = to_torch(np.log(np.maximum(posterior_variance, 1e-20)))
+        self.posterior_mean_coef1 = to_torch(betas * np.sqrt(alphas_cumprod_prev) / (1. - alphas_cumprod))
+        self.posterior_mean_coef2 = to_torch((1. - alphas_cumprod_prev) * np.sqrt(alphas) / (1. - alphas_cumprod))
+        lvlb_weights = self.betas ** 2 / (2 * self.posterior_variance * to_torch(alphas) * (1 - self.alphas_cumprod))
+        lvlb_weights[0] = lvlb_weights[1]
+        self.lvlb_weights = lvlb_weights
+    
     def sample(self,
                S,
-               batch_size,
                shape,
-               conditioning=None,
-               callback=None,
-               normals_sequence=None,
-               img_callback=None,
-               quantize_x0=False,
-               eta=0.,
-               mask=None,
-               x0=None,
-               temperature=1.,
-               noise_dropout=0.,
-               score_corrector=None,
-               corrector_kwargs=None,
-               verbose=True,
+               cond,
+               eta= 1.0,
+               temperature=1.0,
                x_T=None,
-               log_every_t=100,
-               unconditional_guidance_scale=1.,
-               unconditional_conditioning=None, # this has to come in the same format as the conditioning, # e.g. as encoded tokens, ...
-               dynamic_threshold=None,
-               ucg_schedule=None,
-               **kwargs
-               ):
-        S=45
-        batch_size = 4
-        shape = [4, 104, 104]
-        conditioning ={'c_concat': [], 'c_crossattn': []}
-        callback = None
-        normals_sequence = None
-        img_callback = None
-        quantize_x0 = False
-        eta = 1.0
-        mask = None
-        x0 = None
-        temperature = 1.0
-        noise_dropout = 0.0
-        score_corrector = None
-        corrector_kwargs = None
-        verbose = False
-        x_T = "tensor of shape [4,4,104,104]"
-        log_every_t = 100
-        unconditional_guidance_scale = 10
-        unconditional_conditioning = {'c_concat': [], 'c_crossattn': []}
-        dynamic_threshold = None
-        ucg_schedule = None
+               log_every_t=100, 
+               unconditional_guidance_scale=1.0, 
+               unconditional_conditioning=None):
+        
+        #get diffusion timesteps
+        num_ddpm_timesteps = S
+        c = num_ddpm_timesteps // self.dpm_num_timesteps
+        ddim_timesteps = np.asarray(list(range(0, num_ddpm_timesteps, c)))
+        ddim_timesteps = ddim_timesteps + 1
 
-        ctmp = conditioning[list(conditioning.keys())[0]]
-        while isinstance(ctmp, list): ctmp = ctmp[0]
-        cbs = ctmp.shape[0]
-        #making a sampling schedule
+        # calculations for variance schedule
+        alphacums = self.alphas_cumprod.cpu()
+        self.ddim_alphas = alphacums[ddim_timesteps]
+        self.ddim_alphas_prev = np.asarray([alphacums[0]] + alphacums[ddim_timesteps[:-1]].tolist())
+        self.ddim_sqrt_one_minus_alphas = np.sqrt(1. - self.ddim_alphas)
+        self.ddim_sigmas = eta * np.sqrt((1 - self.ddim_alphas_prev) / (1 - self.ddim_alphas) * (1 - self.ddim_alphas / self.ddim_alphas_prev))
+        
+        b = shape[0]
+        img = x_T
+        timesteps = ddim_timesteps
+        intermediates = {'x_inter': [img], 'pred_x0': [img]}
+        time_range = np.flip(timesteps)
+        total_steps = timesteps.shape[0]
+        iterator = tqdm(time_range, desc='DDIM Sampler', total=total_steps)
 
+        for i, step in enumerate(iterator):
+            index = total_steps - i - 1
+            ts = torch.full((b,), step, device=device, dtype=torch.long)
+            outs = self.p_sample_ddim(img, cond, ts, 
+                                    index=index, 
+                                    temperature=temperature,
+                                    unconditional_guidance_scale=unconditional_guidance_scale,
+                                    unconditional_conditioning=unconditional_conditioning,)
+            img, pred_x0 = outs
+            if index % log_every_t == 0 or index == total_steps - 1:
+                intermediates['x_inter'].append(img)
+                intermediates['pred_x0'].append(pred_x0)
+
+        #decode into an image
+        x_samples_ddim = model.decode_first_stage(img)
+        result = torch.clamp((x_samples_ddim + 1.0) / 2.0,min=0.0, max=1.0)
+        result = result.cpu().numpy().transpose(0, 2, 3, 1) * 255
+        return [Image.fromarray(img.astype(np.uint8)) for img in result]
+
+    @torch.no_grad()
+    def p_sample_ddim(self, x, c, t, index, temperature=1.,unconditional_guidance_scale=1., unconditional_conditioning=None):
+        b, *_, device = *x.shape, x.device
+        x_in = torch.cat([x] * 2)
+        t_in = torch.cat([t] * 2)
+        c_in = dict()
+        #batch the conditional and unconditional embeddings together (use?_)
+        for k in c:
+            if isinstance(c[k], list):
+                c_in[k] = [torch.cat([
+                    unconditional_conditioning[k][i],
+                    c[k][i]]) for i in range(len(c[k]))]
+            else:
+                c_in[k] = torch.cat([
+                        unconditional_conditioning[k],
+                        c[k]])
+        xc = torch.cat([x_in] + c_in['c_concat'], dim=1)
+        cc = torch.cat(c_in['c_crossattn'], 1)
+        out = self.diffusion_model(xc, t_in, context=cc)
+        model_uncond, model_t = out.chunk(2)
+        model_output = model_uncond + unconditional_guidance_scale * (model_t - model_uncond)
+        e_t = model_output
+
+        alphas = self.ddim_alphas
+        alphas_prev = self.ddim_alphas_prev
+        sqrt_one_minus_alphas =  self.ddim_sqrt_one_minus_alphas
+        sigmas =  self.ddim_sigmas
+        
+        # select parameters corresponding to the currently considered timestep
+        a_t = torch.full((b, 1, 1, 1), alphas[index], device=device)
+        a_prev = torch.full((b, 1, 1, 1), alphas_prev[index], device=device)
+        sigma_t = torch.full((b, 1, 1, 1), sigmas[index], device=device)
+
+        # current prediction for x_0
+        pred_x0 =  extract_into_tensor(self.sqrt_alphas_cumprod, t, x.shape) * x - extract_into_tensor(self.sqrt_one_minus_alphas_cumprod, t, x.shape) * model_output
+
+        # direction pointing to x_t
+        dir_xt = (1. - a_prev - sigma_t**2).sqrt() * e_t
+        noise = sigma_t * noise_like(x.shape, device, False) * temperature
+        x_prev = a_prev.sqrt() * pred_x0 + dir_xt + noise
+        return x_prev, pred_x0
+    
     def get_conditional(self, text):
         tokens = open_clip.tokenize(text)
         x = self.cond_stage_model.token_embedding(tokens)
@@ -120,26 +207,8 @@ class LatentDiffusionInpainting(nn.Module):
         x = x.permute(1, 0, 2)  # LND -> NLD
         x = self.cond_stage_model.ln_final(x)
         return x
-    def get_learned_conditioning(self, c):
-        c = self.cond_stage_model.encode(c)
-        return c
-    @torch.no_grad()
-    def get_unconditional_conditioning(self, batch_size, null_label=None):
-        if null_label is not None:
-            xc = null_label
-            if isinstance(xc, dict) or isinstance(xc, list):
-                c = self.get_learned_conditioning(xc)
-            else:
-                if hasattr(xc, "to"):
-                    xc = xc.to(self.device)
-                c = self.get_learned_conditioning(xc)
-        if isinstance(c, list):  # in case the encoder gives us a list
-            for i in range(len(c)):
-                c[i] = repeat(c[i], '1 ... -> b ...', b=batch_size).to(self.device)
-        else:
-            c = repeat(c, '1 ... -> b ...', b=batch_size).to(self.device)
-        return c
     
+
 from PIL import Image
 if __name__ == "__main__":
     with torch.no_grad():
@@ -187,6 +256,16 @@ if __name__ == "__main__":
         uc_full = {"c_concat": [c_cat], "c_crossattn": [uc_cross]}
 
         shape = [model.channels , h // 8, w // 8]
+        samples_cfg, intermediates = model.sample(
+            ddim_steps,
+            shape,
+            cond,
+            eta=1.0,
+            unconditional_guidance_scale=scale,
+            unconditional_conditioning=uc_full,
+            x_T=start_code,
+        )
+        
 
 
 
